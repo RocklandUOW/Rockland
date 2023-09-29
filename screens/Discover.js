1637c43b65c7236647b20ca5d43bcd539c86eb7a
Bumskee <fadhlanrazan@gmail.com>
2023-09-29 23:35:50 +1000

    added google map's api and feature to track live location

--- screens/Discover.js
+++ screens/Discover.js
@@ -1,10 +1,48 @@
-import {View, Text} from 'react-native';
-import GlobalStyle from '../styles/GlobalStyle'
+import {View, Text, StyleSheet} from 'react-native';
+import GlobalStyle from '../styles/GlobalStyle';
+import MapView from 'react-native-maps';
+import * as Location from 'expo-location';
+import React, {useState, useEffect} from 'react';
 
 export default function Discover({navigation}) {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== "granted") {
+            setErrorMsg("Permission to access location was denied");
+            return;
+          }
+    
+          let location = await Location.getCurrentPositionAsync({});
+          setLocation(location);
+    
+          let backPerm = await Location.requestBackgroundPermissionsAsync();
+          console.log(backPerm);
+        })();
+      }, []);
+
+    let text = "waiting...";
+
+    if (errorMsg)
+    {
+        text(errorMsg);
+    }
+    else if (location) {
+        text = JSON.stringify(location);
+    }
+
     return (
         <View style={GlobalStyle.center}>
-            <Text> Discover </Text>
+            <MapView
+            showsMyLocationButton={true}
+            showsUserLocation={true}
+            style={StyleSheet.absoluteFill}
+             />
+            {/* <Text> {text} </Text> */}
+
         </View>
     )
 }
