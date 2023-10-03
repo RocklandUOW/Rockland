a7db1b1cd5f67a1f40dc193fc04f0e18f53c97c5
Bumskee <fadhlanrazan@gmail.com>
2023-10-03 22:25:23 +1100

    i do love forgetting to remove the mistakes i made while debugging the fucking application build what the fuck is happening i am getting crazy

--- components/BottomTab.js
+++ components/BottomTab.js
@@ -8,8 +8,8 @@ import { useRef, useState, useEffect } from "react";
 
 // screens imports
 import DiscoverScreen from '../screens/DiscoverScreen'
-import {CameraScreen} from '../screens/CameraScreen'
-import {GalleryScreen} from '../screens/GalleryScreen'
+import CameraScreen from '../screens/CameraScreen'
+import GalleryScreen from '../screens/GalleryScreen'
 
 // the name of the page to be referenced
 const discoverScreenName = 'Discover'
